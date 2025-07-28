5c82f73b2695135eb293d5d6a01d09769e700509
francorinque <francoagustinrinquecorreo@gmail.com>
2025-07-28 17:55:39 -0300

    chore(UI): change ui in footer, add link to my github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,16 @@
 const Footer = () => {
   return (
     <footer className="text-center flex-1 flex items-end justify-center text-1x w-full ">
-      <p>
-        <strong className="ml-2"> {new Date().getFullYear()}</strong>
-        {" - "}
-        Franco Rinque
-      </p>
+      <a
+        href="https://github.com/francorinque"
+        target="_blank"
+        rel="noreferrer"
+        className="flex items-center text-slate-400
+         border border-slate-700 rounded-full p-2 hover:border-slate-500 transition duration-200"
+      >
+        <strong className="ml-2">Developed by Franco Rinque</strong>
+      </a>
     </footer>
-  )
-}
-export default Footer
+  );
+};
+export default Footer;
